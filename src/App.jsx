040beffcbd9d7f041e6d9d4a6c3e2e49919870c9
Dmitry <60594670+YNDmitry@users.noreply.github.com>
2023-12-24 18:31:31 +0500

    p

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,20 @@ import { Bloom, EffectComposer } from '@react-three/postprocessing'
 function App() {
   return (
     <>
+      <div id="trigger1"></div>
+      <div id="trigger2-1"></div>
+      <div id="trigger2-2"></div>
+      <div id="trigger2-3"></div>
+      <div id="trigger2-4"></div>
+      <div id="trigger2-5"></div>
+      <div id="trigger3"></div>
+      <div id="trigger4"></div>
+      <div id="trigger5"></div>
+      <div id="trigger6"></div>
+      <div id="trigger7"></div>
+      <div id="trigger8"></div>
+      <div id="trigger9"></div>
       <div className='canvas-container'>
-        <div id="trigger1"></div>
-        <div id="trigger2-1"></div>
-        <div id="trigger2-2"></div>
-        <div id="trigger2-3"></div>
-        <div id="trigger2-4"></div>
-        <div id="trigger2-5"></div>
-        <div id="trigger3"></div>
-        <div id="trigger4"></div>
-        <div id="trigger5"></div>
-        <div id="trigger6"></div>
-        <div id="trigger7"></div>
-        <div id="trigger8"></div>
-        <div id="trigger9"></div>
         <Canvas camera={{ fov: 45 }} frameloop="demand" shadows>
           {import.meta.env.DEV ? <Stats /> : ''}
           <Suspense fallback={<Loader />}>
